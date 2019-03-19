e0980de4c77cca35a230d5aa5e91d0346f243e5d
Olivandro <aball615@gmail.com>
2019-03-19 22:20:35 +0000

    mistake on my part forgot to git add from root folder... corrected now

--- view/components/header.js
+++ view/components/header.js
@@ -6,7 +6,7 @@ function header(state) {
   return html `
     <header>
       <h1>Agorama Server Co-op Distributive Map</h1>
-      <button class="create-game">info</button>
+      <button class="create-game" hidden>info</button>
     </header>
   `
 }
